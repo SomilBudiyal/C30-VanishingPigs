3f146aae7f6c8b24162cf37f0cd1b81f5ccea6ac
Somil Budiyal <71014859+SomilBudiyal@users.noreply.github.com>
2021-01-05 10:30:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -38,14 +38,14 @@ function setup(){
 
     bird = new Bird(200,50);
 
-    //log6 = new Log(230,180,80, PI/2);
+    
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    //strokeWeight(4);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -63,7 +63,7 @@ function draw(){
 
     bird.display();
     platform.display();
-    //log6.display();
+    
     slingshot.display();    
 }
 
